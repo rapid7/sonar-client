9f503549284573b871c755e97e38bae9a71957d0
pdeardorff-r7 <paul_deardorff@rapid7.com>
2015-04-14 09:50:47 -0700

    Update spec for get_certificate

--- spec/sonar/certificate_spec.rb
+++ spec/sonar/certificate_spec.rb
@@ -5,9 +5,9 @@ describe Sonar::Search do
   let(:client) { Sonar::Client.new }
 
   context "sha1 #get_certificate" do
-    it "should find the Rapid7 certificate by sha1" do
+    it "should find the link to certificate by sha1" do
       res = client.get_certificate(sha1: "1e80c24b97c928bb1db7d4d3c05475a6a40a1186")
-      expect(res.subject.CN).to eq("www.rapid7.com")
+      expect(res._links.self.href).to match(/certificates/)
     end
   end
 end
