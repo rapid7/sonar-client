2dee66dca59a02d5dc448e780269d7c85363a933
pdeardorff-r7 <paul_deardorff@rapid7.com>
2015-11-12 14:35:25 -0800

    Add specs for MS registration POST

---
+++ spec/sonar/registration_spec.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Sonar::Registration do
+  let(:client) { Sonar::Client.new }
+
+  context 'POSTing a valid product key' do
+    let (:resp) { client.register_metasploit("1e80c24b97c928bb1db7d4d3c05475a6a40a1186") }
+
+    it 'responds that the license is valid' do
+      expect(resp).to have_key('valid')
+      expect(resp['valid']).to be(true)
+    end
+    it 'responds with a user email' do
+      expect(resp).to have_key('email')
+      expect(resp['email']).to match(/@/)
+    end
+    it 'responds with an api_key' do
+      expect(resp).to have_key('api_key')
+      expect(resp['api_key']).to match(/KEY/)
+    end
+  end
+end
