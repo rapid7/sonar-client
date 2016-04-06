2881b3db99404d2f0692ea01ed4618adcefb83a0
Diego Augusto Ramos <diego.ramos@adtsys.com.br>
2016-04-06 15:11:32 -0300

    wrapped add permission group

--- lib/sonar/user_group.rb
+++ lib/sonar/user_group.rb
@@ -11,10 +11,12 @@ module Sonar
 
     CREATE_USERGROUP_ENDPOINT = "/api/user_groups/create"
     RM_USERGROUP_PERMISSION_ENDPOINT = "/api/permissions/remove_group"
+    ADD_USERGROUP_PERMISSION_ENDPOINT = "/api/permissions/add_group"
 
     RESERVED_NAMES = %w(anyone)
 
-    # Validates user and pass authentication
+    ##
+    # Create group
     # @param name [String] Name of group
     # @param description [String] optional
     # @return [Hashie::Mash] with attribute group -> <Hashie::Mash id="00" membersCount=0 name="name">
@@ -55,12 +57,47 @@ module Sonar
     def remove_usergroup_permission(permission:, group_name: nil, group_id: nil, project_key: nil, project_id: nil)
       options = basic_authenticated_options
       options[:permission] = permission
-      options[:groupName] = group_name unless group_name.nil?
-      options[:groupId] = group_id unless group_id.nil?
+      options[:groupName]  = group_name  unless group_name.nil?
+      options[:groupId]    = group_id    unless group_id.nil?
       options[:projectKey] = project_key unless project_key.nil?
-      options[:projectId] = project_id unless project_id.nil?
+      options[:projectId]  = project_id  unless project_id.nil?
 
       post(RM_USERGROUP_PERMISSION_ENDPOINT, options)
+    end
+
+    ##
+    # Add a permission to a group
+    #
+    # @param permission [String]
+    # => Possible values for global permissions: admin, profileadmin, gateadmin, shareDashboard, scan, provisioning
+    # => Possible values for project permissions user, admin, issueadmin, codeviewer, scan
+    #
+    # @param group_name [String]
+    # => Group name or 'anyone' (case insensitive)
+    # => Example value: sonar-administrators
+    #
+    # @param group_id [String]
+    # => Group id
+    # => Example value: 42
+    #
+    # @param project_key [String]
+    # => Project key
+    # => Example value: my_project
+    #
+    # @param project_id [String]
+    # => Project id
+    # => Example value: ce4c03d6-430f-40a9-b777-ad877c00aa4d
+    #
+    # @return [String] ""
+    def add_usergroup_permission(permission:, group_name: nil, group_id: nil, project_key: nil, project_id: nil)
+      options = basic_authenticated_options
+      options[:permission] = permission
+      options[:groupName]  = group_name  unless group_name.nil?
+      options[:groupId]    = group_id    unless group_id.nil?
+      options[:projectKey] = project_key unless project_key.nil?
+      options[:projectId]  = project_id  unless project_id.nil?
+
+      post(ADD_USERGROUP_PERMISSION_ENDPOINT, options)
     end
   end
 end
